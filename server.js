e093e77f6cf582799e3a1f8743dd8d81b5fd79d9
Long Hoang <heroprotunlong@gmail.com>
2021-11-22 18:56:44 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+const unirest = require("unirest");
+const API_KEY = "93f14f1d-e2fd-4f15-8dfc-c3c722e15746";
+// import path library
+const path = require('path');
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/associations/:word', (req, res) => {
+	const word = req.params.word;
+	const request = unirest.get(`https://api.wordassociations.net/associations/v1.0/json/search?apikey=${API_KEY}&lang=en&text=${word}`)
+	.then(response => {
+		const results = response.body.response[0].items || []; // grab array of results
+		console.log(`Num results=${results.length}`);
+		res.json(results);
+	})
+	.catch(error => {
+		console.log(`error=${error}`);
+		res.json({status:"Error", message: `${error}`});
+	});
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
+
+app.listen(port, () => {
+  console.log(`word-app listening on port ${port}`);
+});
